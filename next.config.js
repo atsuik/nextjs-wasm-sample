5c7f94a6ce086fb563600e889d24b968837e802e
Atsuro Ikeda <atsik1111@gmail.com>
2023-12-31 13:07:33 +0000

    work wasm fibonacci

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const nextConfig = {
+  webpack(config, { isServer, dev }) {
+    config.plugins.push(
+      new WasmPackPlugin({
+        crateDirectory: path.resolve(__dirname, "./wasm"),
+        outDir: path.resolve(__dirname, "./wasm/pkg"),
+      })
+    );
 
-module.exports = nextConfig
+    // Use the client static directory in the server bundle and prod mode
+    // Fixes `Error occurred prerendering page "/"`
+    config.output.webassemblyModuleFilename =
+      isServer && !dev
+        ? "../static/wasm/[modulehash].wasm"
+        : "static/wasm/[modulehash].wasm";
+
+    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,13 +1,34 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
-
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub fn fib(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fib(n - 2) + fib(n - 1),
+    }
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm!");
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_fib0() {
+        assert_eq!(0, fib(0))
+    }
+    #[test]
+    fn test_fib1() {
+        assert_eq!(1, fib(1))
+    }
+    #[test]
+    fn test_fib3() {
+        assert_eq!(2, fib(3))
+    }
+    #[test]
+    fn test_fib5() {
+        assert_eq!(5, fib(5))
+    }
+    #[test]
+    fn test_fib20() {
+        assert_eq!(6765, fib(20))
+    }
 }
