9b3ee682ae5870e03983a4590c60a4211d56ee28
Atsuro Ikeda <atsik1111@gmail.com>
2024-01-01 09:33:19 +0000

    useEffect

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,33 @@
-'use client';
-import Image from 'next/image'
-import { useState, useEffect ,Suspense } from 'react'
-
+"use client";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [inputNumber, setInputNumber] = useState(0)
+  const [inputNumber, setInputNumber] = useState(0);
   const [fibnacci, setFibnacci] = useState(0);
 
   useEffect(() => {
-  async function _fib() {
-    const  { fib } = await import( "../wasm/pkg");
-    setFibnacci(fib(inputNumber));
-  }
-  _fib();
+    async function _fib() {
+      const { fib } = await import("../wasm/pkg");
+      setFibnacci(fib(inputNumber));
+    }
+    _fib();
   }, [inputNumber]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-      </div>
       <div>
         <h1>Fibonacci</h1>
-          <h2>Input Number: { inputNumber }</h2>
-          <button onClick={() => {(inputNumber > 0) ? setInputNumber(inputNumber - 1): 0}}>-</button>
-          <button onClick={() => setInputNumber(inputNumber + 1)}>+</button>
-          <h2>Fibonacci Nmber: {fibnacci}</h2>
+        <h2>Input Number: {inputNumber}</h2>
+        <button
+          onClick={() => {
+            inputNumber > 0 ? setInputNumber(inputNumber - 1) : 0;
+          }}
+        >
+          -
+        </button>
+        <button onClick={() => setInputNumber(inputNumber + 1)}>+</button>
+        <h2>Fibonacci Nmber: {fibnacci}</h2>
       </div>
     </main>
-  )
+  );
 }
